53e0033dfdcf14846f3d5f4aefb3d45f09ca7810
hadi <rickithadi@gmail.com>
2021-05-30 20:09:12 +0800

    using mongo $lte to compare dates

--- index.js
+++ index.js
@@ -1,46 +1,82 @@
-import express from 'express';
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose';
-import ObjectSchema from './model.js'
-const isDev = process.env.NODE_ENV !== 'production';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import ObjectSchema from "./model.js";
+import parseErrors from "./parseError.js";
+const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
 
 // should probably use env variable for this
-const dbUrl='mongodb+srv://admin:123@cluster0.amcwc.mongodb.net/keyValue?retryWrites=true&w=majority'
-mongoose.set('useFindAndModify', false);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+const dbUrl =
+    "mongodb+srv://admin:123@cluster0.amcwc.mongodb.net/keyValue?retryWrites=true&w=majority";
+mongoose.set("useFindAndModify", false);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 mongoose.connect(dbUrl);
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.get('/', (req, res) => {
-  return res.send('up and running');
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get("/object/:key/:timestamp?", (req, res) => {
+    const date = Date(req.query.timestamp)
+    //check timestamp is provided and valid
+    if (date) {
+        ObjectSchema.find({
+                createdAt: {
+                    $lte: date
+                }
+            })
+            .sort({
+                '_id': -1
+            }).limit(1).then(data => res.json({
+                data
+            }))
+            .catch(err => res.status(400).json({
+                errors: parseErrors(err.errors)
+            }));
+
+    } else {
+        //look for object with corresponding key and return latest
+        ObjectSchema.find({
+                key: req.params.key
+            }).sort({
+                '_id': -1
+            }).limit(1).then(data => res.json({
+                data
+            }))
+            .catch(err => res.status(400).json({
+                errors: parseErrors(err.errors)
+            }));
+    }
 });
-app.get('/keys', (req, res) => {
-  return res.send('getting all data');
+
+app.post("/object", (req, res) => {
+    //create object from schema and insert into db
+    //return said object
+    const key = Object.keys(req.body)[0];
+    ObjectSchema.create({
+            value: req.body[key],
+            key
+        })
+        .then((object) => res.json({
+            object
+        }))
+        .catch((err) =>
+            res
+            .status(400)
+            .json({
+                errors: parseErrors(err.errors)
+            })
+        );
 });
 
-
-app.post('/object', (req, res) => {
-	//create object from schema and insert
-let key=	Object.keys(req.body)[0]
-	ObjectSchema.create({value:req.body[key],key})
-	 //   .then(object => res.json({object}))
-	    //.catch(err => res.status(400).json({err}));
-
-	//return said object
-  return res.send('Received a POST HTTP method');
-});
-
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
-});
-
-
-app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT}!`));
+app.listen(PORT, () =>
+    console.log(`listening on port ${PORT}`)
+);
