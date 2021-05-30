f918f4992e398f0eda5b8e65081e3d130da50e50
hadi <rickithadi@gmail.com>
2021-05-30 18:39:56 +0800

    connecting to mongo instance

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import express from 'express';
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose';
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
+// should probably use env variable for this
+const dbUrl='mongodb+srv://admin:123@cluster0.amcwc.mongodb.net/keyValue?retryWrites=true&w=majority'
+mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(dbUrl);
+
 const app = express();
-
 app.use(cors());
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-function allKeys() {
-    return [
-        {id: 1, name: "Finish writing a blogpost"},
-        {id: 2, name: "Make chickpea curry for dinner"},
-        {id: 3, name: "Wake up at 5:30am"},
-    ];
-}
 app.get('/', (req, res) => {
   return res.send('up and running');
 });
 app.get('/keys', (req, res) => {
-  return res.send(allKeys());
+  return res.send('getting all data');
 });
 
 
